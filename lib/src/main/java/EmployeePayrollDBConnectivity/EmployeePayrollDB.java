0e78ebbd0e7a4fadd7d032036ac07414995662ef
nikita1803 <rainikita595@gmail.com>
2021-06-20 23:47:58 +0530

    Added: Connection is stablish with the database

--- lib/src/main/java/EmployeePayrollDBConnectivity/EmployeePayrollDB.java
+++ lib/src/main/java/EmployeePayrollDBConnectivity/EmployeePayrollDB.java
@@ -10,6 +10,42 @@ public class EmployeePayrollDB
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome in Employee payroll database connectivity");
+		String jdbcURL = "jdbc:mysql://localhost/payroll_service?characterEncoding=latin1";;
+		String userName = "root";
+		String password = "Nikita007@";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Drive loaded");
+		}
+		catch(ClassNotFoundException e)
+		{
+			throw new IllegalStateException("Cannot find athe driver in the classpath!", e);
+		}
+		listDrivers();
+		try {
+			System.out.print("Connection databse:" +jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName , password);
+			System.out.println("Connection is successful!!!!!!"+connection);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This function is use to show the driver which is installed.
+	 */
+	private static void listDrivers() 
+	{
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while(driverList.hasMoreElements())
+		{
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("  "+driverClass.getClass().getName());
+		}
+
 	}
 		
 }
